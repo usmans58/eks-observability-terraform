dae188198f4b05526bf8e0ec29c289c558c061c7
Usman Siddiqui <usmansiddiqui51@gmail.com>
2025-06-28 06:41:59 +0500

    added nat gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,28 +15,3 @@ resource "aws_internet_gateway" "eks_igw" {
   }
 }
 
-resource "aws_subnet" "eks_private_subnet" {
-  count = 2
-  vpc_id = aws_vpc.eks_vpc.id
-  cidr_block = var.private_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
-
-  tags = {
-    Name = "${var.cluster_name}-private-subnet-${count.index + 1}"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-  }
-}
-resource "aws_subnet" "eks_public_subnet" {
-  count = 2
-  vpc_id = aws_vpc.eks_vpc.id
-  map_public_ip_on_launch = true
-  cidr_block = var.public_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
-
-  tags = {
-    Name = "${var.cluster_name}-public-subnet-${count.index + 1}"
-    "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-  }
-}
