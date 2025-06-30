40c5c6f736b1bd54f3dc386267ec12b9ba5c3ff9
Usman Siddiqui <usmansiddiqui51@gmail.com>
2025-07-01 02:57:41 +0500

    added developer role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -23,6 +23,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+//------------------------------------------------------------------------------------------------
 
 resource "aws_iam_role" "node_group_role" {
   name = "${var.cluster_name}-node-group-role"
@@ -56,4 +57,40 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
 resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
   role       = aws_iam_role.node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+//------------------------------------------------------------------------------------------------
+
+resource "aws_iam_user" "developer_role" {
+  name = "${var.cluster_name}-developer-role"
+  tags = {
+    Name = "${var.cluster_name}-developer-role"
+  }
+}
+
+resource "aws_iam_policy" "developer_policy" {
+  name   = "${var.cluster_name}-developer-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListClusters"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+resource "aws_iam_user_policy_attachment" "developer_policy_attachment" {
+  user       = aws_iam_user.developer_role.name
+  policy_arn = aws_iam_policy.developer_policy.arn
+}
+
+resource "aws_eks_access_entry" "developer_access" {
+  cluster_name = var.cluster_name
+  principal_arn = aws_iam_user.developer_role.arn
+  kubernetes_groups = ["viewer"]
 }
