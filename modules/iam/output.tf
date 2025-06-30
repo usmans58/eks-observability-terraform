139a083a47147ff60d68dfc0cca7c0e6723823ac
Usman Siddiqui <usmansiddiqui51@gmail.com>
2025-06-30 06:06:15 +0500

    added node group IAM output

--- modules/iam/output.tf
+++ modules/iam/output.tf
@@ -3,3 +3,8 @@ output "eks_cluster_role_arn" {
   value       = aws_iam_role.eks_cluster_role.arn
 
 }
+output "node_group_role_arn" {
+  description = "The ARN of the EKS node group role"
+  value       = aws_iam_role.node_group_role.arn
+
+}
